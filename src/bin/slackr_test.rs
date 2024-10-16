1cde90c091a775b001dadbce50640d00539082df
mnemonad <60589750+mnemonad@users.noreply.github.com>
2024-10-15 22:04:43 -0500

    Event handler demo
    Simple callback example

--- src/bin/slackr_test.rs
+++ src/bin/slackr_test.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::path::Path;
-use slackr::client::SlackClient;
+use slackr::client::{ SlackClient, SlackEnvelope };
 
 use futures_util::stream::StreamExt;
 
@@ -16,7 +16,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let _ = client.connect_to_socket(None).await;
 
-    client.listen().await; 
+    client.register_callback("message", |event: SlackEnvelope| {
+        Box::pin(async move {
+            let msg_event = &event.payload.event;
+            println!("Received message: {:?} from {:?} in {:?}", msg_event.text, msg_event.user, msg_event.channel);
+        })
+    });
+
+    client.listen().await;
 
     Ok(())
 }
